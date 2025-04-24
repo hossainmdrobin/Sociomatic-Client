e252a2ad2b0139960eeb1b33058ad5decef362c1
Robin <hossainmdrobin967@gmail.com>
2025-04-24 16:25:59 -0600

    couldnot happen

--- src/components/partials/SocialLogin/SocialLoginButtons.tsx
+++ src/components/partials/SocialLogin/SocialLoginButtons.tsx
@@ -21,13 +21,13 @@ const socialButtons = [
 ]
 
 export default function SocialLoginButtons() {
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
     return (
         <div>
-            <TriggerButton onClick={handleOpen}>Open modal</TriggerButton>
+            {/* <TriggerButton onClick={handleOpen}>Open modal</TriggerButton> */}
             <Modal
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
